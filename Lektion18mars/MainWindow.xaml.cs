173764f5d3137f0a17d25e066e9ca0ad94c9aeb1
Adam Folke <Adam.Folke@hudiksvall.se>
2024-03-11 18:06:33 +0100

    Add project files.

---
+++ Lektion18mars/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Lektion18mars
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private ObservableCollection<Uppgifter> tasks = new ObservableCollection<Uppgifter>();
+        public ObservableCollection<Uppgifter> Tasks
+        {
+            get { return tasks; }
+            set
+            {
+                tasks = value;
+                OnPropertyChanged(nameof(Tasks));
+            }
+        }
+
+        private Uppgifter newTask;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public Uppgifter NewTask
+        {
+            get { return newTask; }
+            set
+            {
+                newTask = value;
+                OnPropertyChanged(nameof(NewTask));
+            }
+        }
+
+        public string tid { get; set; } = DateTime.Now.ToString("HH,mm");
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(newTaskTextBox.Text))
+            {
+                Tasks.Add(new Uppgifter(newTaskTextBox.Text, alarmDatePicker.Text));
+                newTaskTextBox.Clear();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Uppgifter selectedTask = (Uppgifter)taskListBox.SelectedItem;
+            if (selectedTask != null)
+            {
+                Tasks.Remove(selectedTask);
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Lektion18mars/Uppgifter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lektion18mars
+{
+    public class Uppgifter
+    {
+        public string Uppgift { get; set; }
+
+        public string Datum { get; set; }
+
+        public Uppgifter(string uppgift, string datum)
+        {
+            Uppgift = uppgift;
+            Datum = datum;
+        }
+    }
+}
